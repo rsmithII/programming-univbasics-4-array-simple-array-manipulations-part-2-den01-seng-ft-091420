358e19417832a7f6a872fb024531d20ada15f1e3
Rodney Smith II <r_smith3@hotmail.com>
2020-08-25 03:55:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ end
 
 def using_delete(instructors)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  instructors.delete(3, "Steven")
+  instructors.delete("Steven")
 end
 
 def using_delete_at(famous_robots)
